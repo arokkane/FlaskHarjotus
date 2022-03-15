8a60a0f6b7ee3537f36e9244ef1d2be2914cc331
August Rokkanen <arokkane1@gmail.com>
2022-03-15 09:52:54 +0200

    basic done

--- website/__init__.py
+++ website/__init__.py
@@ -10,6 +10,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'asgasjgaowqertq'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
 
@@ -22,7 +23,7 @@ def create_app():
     from .models import User, Note
 
     create_database(app)
-    
+
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
