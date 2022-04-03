83cfb6c49b9c3bc1df3ac7beb379de26135985eb
August Rokkanen <arokkane1@gmail.com>
2022-04-03 15:48:10 +0300

    new forms

--- website/forms.py
+++ website/forms.py
@@ -1,11 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateTimeField, SelectField
-from wtforms.validators import DataRequired
-GAME_CHOICES = [('0', 'Guilty Gear Strive'),('1', 'BlazBlue Centralfiction'), ('3', 'Street Fighter V0')]
+from wtforms import StringField, SubmitField, DateTimeField, SelectField, DateTimeLocalField
+from wtforms.validators import DataRequired, InputRequired, Length
+GAME_CHOICES = [('0', 'Guilty Gear Strive'),('1', 'BlazBlue Centralfiction'), ('2', 'Street Fighter V0')]
 
 class MyForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
 
 class EventCreationFrom(FlaskForm):
-    name = StringField('Event Name', validators=[DataRequired()])
+    name = StringField('Event Name', validators=[InputRequired(), Length(min=1, max=200)])
     game = SelectField(label='Select Game', choices=GAME_CHOICES)
+    application_period = DateTimeLocalField(label='Application Period', validators=[InputRequired()])
+    event_time = DateTimeLocalField(label='Event Date', validators=[InputRequired()])
--- website/views.py
+++ website/views.py
@@ -1,9 +1,10 @@
+from importlib.metadata import requires
 import json
 from flask import Blueprint, flash, jsonify, redirect, render_template, request, url_for
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
-from .forms import MyForm
+from .forms import MyForm, EventCreationFrom
 
 views = Blueprint('views', __name__)
 
@@ -34,10 +35,11 @@ def delete_note():
     return redirect(url_for('views.home'))
 @views.route('/create-event', methods=['GET', 'POST'])
 def create_event():
+    form = EventCreationFrom(request.form)
     if request.method == 'POST':
         pass
         #implement event creation
-    return render_template("create_event.html", user=current_user)
+    return render_template("create_event.html", user=current_user, form=form)
 @views.route('/application', methods=['GET', 'POST'])
 def application():
     form = MyForm()
